0d0ff1fd54f9532e937c4874216d7aa9a3cb3c7f
Isaiah peter <peterisaiah4fun@gmail.com>
2023-02-28 20:32:47 +0100

    task 5 to 7

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
+from typing import Union
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
@@ -14,6 +23,30 @@ class Config(object):
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@app.before_request
+def before_request(login_as: int = None):
+    """ Request of each function
+    """
+    user: dict = get_user()
+    print(user)
+    g.user = user
+
+
+def get_user() -> Union[dict, None]:
+    """ Get the user of the dict
+        Return User
+    """
+    login_user = request.args.get('login_as', None)
+
+    if login_user is None:
+        return None
+
+    user: dict = {}
+    user[login_user] = users.get(int(login_user))
+
+    return user[login_user]
 
 
 @babel.localeselector
@@ -36,7 +69,7 @@ def hello_world():
         Return:
             Initial template html
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
